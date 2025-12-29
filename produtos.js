0b6baaf307e47c43494a9d3b6f7554db77dac88d
chriscell1956 <christianorgdo@gmail.com>
2025-12-29 00:15:07 -0300

    Upload inicial do site

---
+++ produtos.js
@@ -0,0 +1,570 @@
+window.produtos_db = [
+    {
+        "id": "MFM10531",
+        "nome": "Kit Medalhão NS Guadalupe - Kit Faça vc mesmo",
+        "preco": "52,20",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-766ec987f46ddeb0e317655416629878-480-0.webp"
+    },
+    {
+        "id": "MFM10529",
+        "nome": "Combo Projeto 110 Clube - Escapulário São José",
+        "preco": "58,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-7085b9e70857452a5317655381200361-480-0.webp"
+    },
+    {
+        "id": "MFM10528",
+        "nome": "Combo Projeto 109 Clube - Quadro Sagrado Coração",
+        "preco": "36,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/1-399c93aa1ff16c5a5d17655382547707-480-0.webp"
+    },
+    {
+        "id": "MFM10527",
+        "nome": "Combo Projeto 108 Clube - Pêndulo Mãezinha Luxo",
+        "preco": "48,20",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-f9b75c320d0909adb717655389700203-480-0.webp"
+    },
+    {
+        "id": "MFM10526",
+        "nome": "Combo Projeto 107 Clube - Quadro Sagrada Família Bordado",
+        "preco": "56,20",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/3-2e60a00eb2ead029bd17655390777309-480-0.webp"
+    },
+    {
+        "id": "MFM10525",
+        "nome": "Conjunto Cruz Entremeio Emborrachado Verde",
+        "preco": "8,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/10-c2bf583882953f335617635594833717-480-0.webp"
+    },
+    {
+        "id": "MFM10438",
+        "nome": "Combo Projeto 98 Clube Nicho NS Aparecida",
+        "preco": "48,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-057f5a1c467696c59817581393753276-480-0.webp"
+    },
+    {
+        "id": "MFM10436",
+        "nome": "Combo Projeto 96 Clube Altar Arabesco NS Nazaré",
+        "preco": "48,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/1-7baafa6ea811c65d2917575886096822-480-0.webp"
+    },
+    {
+        "id": "MFM10434",
+        "nome": "Combo Projeto 95 Clube NS Nazaré",
+        "preco": "48,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/3-b098fdcee7fa39acee17563859840739-480-0.webp"
+    },
+    {
+        "id": "MFM10433",
+        "nome": "Combo Projeto 94 Clube Altar Santa Teresinha",
+        "preco": "48,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-0f4c0111d67187811517563859010780-480-0.webp"
+    },
+    {
+        "id": "MFM10364",
+        "nome": "Combo Projeto 91 Clube Flor em perola - 10un",
+        "preco": "39,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-06d914ee9d2655dcaa17538780354607-480-0.webp"
+    },
+    {
+        "id": "MFM10280",
+        "nome": "Combo Projeto 85 Clube Pendulo Arabesco São Bento",
+        "preco": "48,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/copia-de-fotos-aviamentos-2-fae30270084f1b5c9e17501970642845-480-0.webp"
+    },
+    {
+        "id": "MFM10277",
+        "nome": "Combo Projeto 84 Clube Quadro São Bento",
+        "preco": "52,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/5-d0a342df7e073219e417497453007733-480-0.webp"
+    },
+    {
+        "id": "MFM10218",
+        "nome": "Combo Projeto 79 Clube - Pendulo Santa Rita",
+        "preco": "38,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-580b8f884d4e413e5d17467291061050-480-0.webp"
+    },
+    {
+        "id": "MFM10220",
+        "nome": "Combo Projeto 80 Clube - Quadro Santa Rita",
+        "preco": "34,20",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-3bc143edb5b224f9c717471554903607-480-0.webp"
+    },
+    {
+        "id": "MFM10194",
+        "nome": "Combo Projeto 74 Clube Pendulo NS Fatima",
+        "preco": "38,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/copia-de-fotos-aviamentos-1-00a6d169ddfae1dd5717433635362639-480-0.webp"
+    },
+    {
+        "id": "MFM259",
+        "nome": "Combo Projeto 69 Clube Guirlanda Coelho",
+        "preco": "58,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/3-31045d6a238438c5d617404197344898-480-0.webp"
+    },
+    {
+        "id": "MFM258",
+        "nome": "Combo Projeto 68 Clube Guirlanda Pascoa Rustica",
+        "preco": "58,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/1-133f0d781e683946f717404198319577-480-0.webp"
+    },
+    {
+        "id": "FM702",
+        "nome": "Entremeio e cruz MDF Terço parede",
+        "preco": "8,00",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/96-a058c5595b2f5a02dc17102676896610-480-0.webp"
+    },
+    {
+        "id": "FM563",
+        "nome": "Base Suporte MDF",
+        "preco": "6,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/851-63b1c6192adc4dd64e16938332372693-480-0.webp"
+    },
+    {
+        "id": "FM329",
+        "nome": "Divino 5cm MDF - Pcte 03 un",
+        "preco": "6,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/941-c81f3f0e61f939627516613416825664-480-0.webp"
+    },
+    {
+        "id": "FM328",
+        "nome": "Aparecida MDF - Pcte 03 un",
+        "preco": "5,60",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/931-9c8d8b6729c5c5274516613415953951-480-0.webp"
+    },
+    {
+        "id": "FM327",
+        "nome": "Anjo MDF - Pct 03 un",
+        "preco": "5,60",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/921-c9df2f5848ab9c417816613415502055-480-0.webp"
+    },
+    {
+        "id": "FM260",
+        "nome": "Base em mdf para molduras",
+        "preco": "18,00",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/9611-7ff23dcd5f54c67feb16449325299521-480-0.webp"
+    },
+    {
+        "id": "FM219",
+        "nome": "Tripé MDF",
+        "preco": "6,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/981-3df08759a1e77e3ed516410497328575-480-0.webp"
+    },
+    {
+        "id": "MFM204",
+        "nome": "Aureola acrílico P - 01 un",
+        "preco": "3,00",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/66-87b72497bea248095b17403540035251-480-0.webp"
+    },
+    {
+        "id": "FM498",
+        "nome": "Divino P - Pcte 3 un acrílico branco",
+        "preco": "6,00",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/771-5c7e96b90fa29a9a4c16921270129716-480-0.webp"
+    },
+    {
+        "id": "FM112",
+        "nome": "Anjo acrílico dourado - Pcte 3 un",
+        "preco": "11,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/151-ae4d6a91126743b16016408836708537-480-0.webp"
+    },
+    {
+        "id": "MFM10274",
+        "nome": "Meu Batizado Branco - Pcte 3 un",
+        "preco": "14,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-193eb147759c8a2fc417496438600762-480-0.webp"
+    },
+    {
+        "id": "FM10133",
+        "nome": "Base branca com argola acrilico - Pcte 03 peças",
+        "preco": "11,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/7-b33e1d994b1dc1503817375031051147-480-0.webp"
+    },
+    {
+        "id": "MFM10524",
+        "nome": "Conjunto Cruz Entremeio Emborrachado Azul",
+        "preco": "8,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/11-18bc097ff14d481b1017635593628261-480-0.webp"
+    },
+    {
+        "id": "MFM10519",
+        "nome": "Adesivo Sagrada Família - Pct 03 un",
+        "preco": "8,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/1-d004cd772c34d62d1717635591264091-480-0.webp"
+    },
+    {
+        "id": "MFM10520",
+        "nome": "Adesivo São Miguel - Pct 03 un",
+        "preco": "8,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/4-fae7cea5c3df0d24ab17635592209384-480-0.webp"
+    },
+    {
+        "id": "MFM10514",
+        "nome": "Pingente de Perola gota - 06un",
+        "preco": "13,70",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/5-f56a7f0db7f92cfadd17635548152428-480-0.webp"
+    },
+    {
+        "id": "MFM10506",
+        "nome": "Guardanapo Decoupagem Borboletas",
+        "preco": "4,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-731338aacf3609d88e17635536657348-480-0.webp"
+    },
+    {
+        "id": "MFM10511",
+        "nome": "Guardanapo Decoupagem Flor Lavanda",
+        "preco": "4,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/12-b97792e35a7704f0f417635539126117-480-0.webp"
+    },
+    {
+        "id": "MFM10510",
+        "nome": "Guardanapo Decoupagem Flor aquarela",
+        "preco": "4,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/10-5fa1cd74b27cbc037e17635537999219-480-0.webp"
+    },
+    {
+        "id": "MFM10509",
+        "nome": "Guardanapo Decoupagem Flor borda rosa",
+        "preco": "4,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/8-e1a72204f82f5b215217635537348448-480-0.webp"
+    },
+    {
+        "id": "MFM10508",
+        "nome": "Guardanapo Decoupagem Flor mista",
+        "preco": "4,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/6-d6dbfa610be47b88b517635537037110-480-0.webp"
+    },
+    {
+        "id": "MFM10507",
+        "nome": "Guardanapo Decoupagem Flor Lilas/azul",
+        "preco": "4,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/4-a11bc8cb098d2169ad17635535918598-480-0.webp"
+    },
+    {
+        "id": "MFM10498",
+        "nome": "Medalha NS das Graças oval acrílico o gravada",
+        "preco": "24,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/6-d156f2392153e5f18817635497960237-480-0.webp"
+    },
+    {
+        "id": "MFM10497",
+        "nome": "Medalha NS Aparecida oval acrílico o gravada",
+        "preco": "24,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/10-a15beee6aab1aa2cf617635497225165-480-0.webp"
+    },
+    {
+        "id": "MFM10496",
+        "nome": "Cruz mdf trabalhado 1un",
+        "preco": "14,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/4-67d1d3120d1c7098b717635496133563-480-0.webp"
+    },
+    {
+        "id": "MFM10494",
+        "nome": "Pedra Cristal Sextavada n30 - 06un",
+        "preco": "19,40",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-7b30635a0e393d0e3717635487459429-480-0.webp"
+    },
+    {
+        "id": "MFM10492",
+        "nome": "Meia perola n20- Pcte 12un",
+        "preco": "5,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/3-061ba5bc7810c94c9a17634871423118-480-0.webp"
+    },
+    {
+        "id": "MFM10505",
+        "nome": "Ponteira com argola marrom",
+        "preco": "18,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/8-969d74735b5bdbbfa817635529127111-480-0.webp"
+    },
+    {
+        "id": "MFM10504",
+        "nome": "Ponteira com argola oval branca",
+        "preco": "18,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/6-0fba4caa349940c34817635528780965-480-0.webp"
+    },
+    {
+        "id": "MFM10503",
+        "nome": "Ponteira com argola oval transparente",
+        "preco": "18,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/4-72ad3d437c330f5acf17635528468633-480-0.webp"
+    },
+    {
+        "id": "MFM10502",
+        "nome": "Ponteira com argola marmore",
+        "preco": "18,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-bf897f31d49ab839e917635527778122-480-0.webp"
+    },
+    {
+        "id": "MFM10501",
+        "nome": "Base acrilico Sagrado Coração",
+        "preco": "22,20",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-a5f5fa3fb3db4f05b717635505637991-480-0.webp"
+    },
+    {
+        "id": "MFM10408",
+        "nome": "Adesivo Têxtil - Floral Ramalhete FM08",
+        "preco": "19,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/4-4cca6a49ae53ff511317551919290608-480-0.webp"
+    },
+    {
+        "id": "MFM10407",
+        "nome": "Adesivo Têxtil - Floral Amarelo FM07",
+        "preco": "19,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-5e7750412ebc2237a717551917916077-480-0.webp"
+    },
+    {
+        "id": "MFM10381",
+        "nome": "Prensa Térmica Rosa",
+        "preco": "120,00",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/3-e39579a9099ea1d70f17545015354338-480-0.webp"
+    },
+    {
+        "id": "MFM10347",
+        "nome": "Ns Aparecida 19cm gesso Bordada",
+        "preco": "25,00",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/5-312be0f7aa17329c1917526695023985-480-0.webp"
+    },
+    {
+        "id": "MFM10315",
+        "nome": "Kit Suporte Altar Resinado - Você escolhe a imagem - Kit faça você mesmo",
+        "preco": "23,40",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/8-7425d493e76975fa2f17509572287599-480-0.webp"
+    },
+    {
+        "id": "MFM10258",
+        "nome": "Aplique São Bento 5,5cm Acrílico Dourado - Pcte 3 un",
+        "preco": "12,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/2-2f650009795f44f22217491290859257-480-0.webp"
+    },
+    {
+        "id": "FM10212",
+        "nome": "Plaquinha Imagem NS Graças - Pcte 03un",
+        "preco": "11,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/29-ebd730e296a0d0058217485178711246-480-0.webp"
+    },
+    {
+        "id": "MFM10252",
+        "nome": "Perola inteira n30 - Pcte com 6 unidades",
+        "preco": "16,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/5-2594a39e3497696a9517484438793082-480-0.webp"
+    },
+    {
+        "id": "MFM10231",
+        "nome": "Adesivo NS Aparecida - Pct 03 un",
+        "preco": "8,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/8-46d1833ed9e322a4d317471588129274-480-0.webp"
+    },
+    {
+        "id": "MFM10230",
+        "nome": "Adesivo Santa Terezinha- Pct 03 un",
+        "preco": "8,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/4-885f27e9cc3ed1c05e17471586733846-480-0.webp"
+    },
+    {
+        "id": "FM10179",
+        "nome": "Adesivo Decoupagem Floral Azul",
+        "preco": "28,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/7-1d27a4f5c2e4695d1f17406140561025-480-0.webp"
+    },
+    {
+        "id": "FM10175",
+        "nome": "Guardanapo Decoupagem Floral Colorido",
+        "preco": "4,20",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/3-cd80c68576d61704e617406142953122-480-0.webp"
+    },
+    {
+        "id": "MFM226",
+        "nome": "Kit Mandalinha Bordada Imagem - Kit faça vc mesmo",
+        "preco": "68,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/27-e94ee6cd960c50263c17403592010599-480-0.webp"
+    },
+    {
+        "id": "MFM215",
+        "nome": "Kit Medalhão Arabesco Imagem - Kit Faça vc mesmo",
+        "preco": "54,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/12-c19d071d29f7b2d95b17403597855558-480-0.webp"
+    },
+    {
+        "id": "MFM149",
+        "nome": "Kit Chaveiro Mármore Nossa Senhora Aparecida - Kit faça vc mesmo",
+        "preco": "28,90",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/8-b0210f4d705d2aab4217403521462643-480-0.webp"
+    },
+    {
+        "id": "MFM155",
+        "nome": "Medalha Oval Resinada Ouro Velho",
+        "preco": "9,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/12-9f6b4a76d224d72fe317403519632626-480-0.webp"
+    },
+    {
+        "id": "MFM166",
+        "nome": "Medalha Retrato Resinada Ouro Velho",
+        "preco": "9,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/22-c827bee0e0aa467cf717403517880077-480-0.webp"
+    },
+    {
+        "id": "MFM160",
+        "nome": "Medalha Oval Resinada Prata",
+        "preco": "9,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/17-16139137b1759df08817403518618104-480-0.webp"
+    },
+    {
+        "id": "MFM054",
+        "nome": "Kit Cupula Bordada - Kit Faça vc mesmo",
+        "preco": "118,00",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/1-783327de95b9a507db17402370739978-480-0.webp"
+    },
+    {
+        "id": "FM10117",
+        "nome": "Combo Workshop Celebrações - Aula 04 Colar Mesa Divino",
+        "preco": "86,80",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/4-bec356000ab103228717362592252742-480-0.webp"
+    },
+    {
+        "id": "FM10101",
+        "nome": "Combão kit escapulário MDF colorido",
+        "preco": "89,70",
+        "categoria": "Geral",
+        "destaque": true,
+        "imagem": "stores/001/977/761/products/153-5593472f7cd2a0189117325345800036-480-0.webp"
+    }
+];
